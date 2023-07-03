1f72a25e90d6b9cb1381aa5a953f041e58d7fd84
Ma Zl <yee@yee.bar>
2023-07-03 12:56:19 +0800

    bbb

--- app.py
+++ app.py
@@ -27,6 +27,7 @@ bot = Bot(bot_id=os.environ.get('bot_id'), bot_secret=os.environ.get('bot_secret
 # 初始化Bot，填写你的bot_id、密钥以及回调地址endpoint
 # 举例：若申请时提供的回调地址为https://域名/callback，这里的callback_url就填`/callback`
 
+
 @bot.on_startswith("ping",prefix='/')
 async def alive(event: SendMessageEvent):
     await event.send('pong',mention_sender=True)
